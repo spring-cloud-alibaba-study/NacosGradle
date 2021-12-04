04687c1250d2aa79a6561e59887de7fef450b803
姜爽 <2545251075@qq.com>
2021-12-04 20:39:50 +0800

    fix ->修正

--- common/src/main/java/com/js/enums/ExceptionEnum.java
+++ common/src/main/java/com/js/enums/ExceptionEnum.java
@@ -16,7 +16,7 @@ public enum ExceptionEnum implements ErrorTypeException {
     /**
      * 网络异常
      **/
-    NETWORH_ANOMALY_ERRORTYPE("NETWORH_ANOMALY_ERRORTYPE", "网络异常,请稍后再试"),
+    NETWORK_ANOMALY_ERRORTYPE("NETWORK_ANOMALY_ERRORTYPE", "网络异常,请稍后再试"),
     /**
      * 当出现参数异常时返回的code码
      */
